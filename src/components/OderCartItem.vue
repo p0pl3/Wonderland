aea9ab5be03d32498ce73728d9afac6cb2e1481b
p0pl3 <m.poplavskiy04@gmail.com>
2022-12-23 15:31:12 +0300

    deploy

--- src/components/OderCartItem.vue
+++ src/components/OderCartItem.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="basket__item">
-    <img class="basket__item-image" src="@/assets/product_1.jpeg">
+    <img class="basket__item-image" :src="require(`@/assets/${product.img}`)">
     <div class="basket__item-description">
-      <span>Toy</span>
+      <h3>{{ product.title }}</h3>
+
     </div>
-    <div class="basket__item-price">1000 $</div>
+    <div class="basket__item-price">
+      <h4>{{ product.quantity }} x {{ product.new_price }}$</h4>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "BasketItem"
+  name: "BasketItem",
+  props: ["product"]
 }
 </script>
 
 <style scoped>
 .basket__item {
   display: flex;
-  align-items: center;
   margin-bottom: 20px;
+  align-items: center;
 }
 
 .basket__item-image {
@@ -27,18 +31,21 @@ export default {
   align-items: center;
   align-content: center;
   background: white;
-  flex: 0 1 100px;
   object-fit: contain;
+  flex: 0 1 100px;
 }
 
 .basket__item-description {
-  flex: 1 1 content;
   font-size: 16px;
   padding: 0 10px;
+  flex:1 1 90px;
 }
 
 .basket__item-price {
-  text-align: end;
-  font-weight: bold;
+  text-align: right;
+}
+
+h3, h4 {
+  margin: 0;
 }
 </style>
